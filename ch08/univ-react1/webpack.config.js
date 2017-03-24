b7573d0cba53f91dc3e34d4ee6af859dab590aa5
Hyungseob Won <stepanowon@hotmail.com>
2017-03-24 11:36:06 +0900

    Update webpack.config.js

--- ch08/univ-react1/webpack.config.js
+++ ch08/univ-react1/webpack.config.js
@@ -3,43 +3,16 @@ var webpack = require('webpack');
 module.exports = {
   devtool : 'source-map',
   entry: [
-    './index.js'
+    './browser.js'
   ],
   output: {
-    path: '../../../../../build/resources/main/static/scripts/',
+    path: './public',
     filename: 'bundle.js'
   },
-   module : {
-        loaders : [
-            { test:/\.json$/, loader : 'json-loader' },
-            { test:/\.js$/, exclude:/node_modules/, loader:'babel-loader' },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader?modules',
-                    'css-loader?modules',
-                    {
-                        loader : 'postcss-loader',
-                        options : {
-                            plugins : function() {
-                                return [
-                                    require('precss'),
-                                    require('autoprefixer')
-                                ];
-                            }
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins : [
-       new WebpackBrowserPlugin()
-    ],
-    devServer : {
-        contentBase : './public',
-        inline:true,
-        historyApiFallback : true,
-        port : 7777
-    }
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader'
+    }]
+  }
 };
