a0e4aef99d39981349d7e2c6fb5948b95ee12b60
Hyungseob Won <stepanowon@hotmail.com>
2017-09-12 08:03:40 +0900

    Update webpack.config.js

--- ch01/hello01/webpack.config.js
+++ ch01/hello01/webpack.config.js
@@ -1,18 +1,60 @@
+var WebpackBrowserPlugin = require('webpack-browser-plugin');
+var webpack = require('webpack');
+
 module.exports = {
-    entry :  __dirname + '/source/App.js',
-    output : {
-        path : __dirname + '/public',
-        filename : 'bundle.js'
+    devtool : 'source-map', 
+    entry: __dirname + '/source/App.js', 
+    output: { 
+        path: __dirname + '/public', 
+        filename: 'bundle.js' 
     },
-    module : {
-        loaders : [
-            { test:/\.js$/, exclude:/node_modules/, loader:'babel-loader' },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: 'css-loader', options: { modules: 1 } },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                          plugins: (loader) => [
+                            require('autoprefixer')(),
+                            require('precss')()
+                          ]
+                        }
+                    }
+                ]
+            }
+        ],
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
         ]
     },
+    plugins : [
+       new webpack.BannerPlugin("Date : 2017.09.02"),
+       new webpack.BannerPlugin("Author : Stephen Won"),
+       new webpack.BannerPlugin("Copyright OpenSG inc."),
+       new webpack.HotModuleReplacementPlugin()       
+    ],
     devServer : {
-        contentBase : __dirname + '/public',
+        contentBase : './public',
+        inline:true, 
         historyApiFallback :true,
-        inline:true,
-        port : 7777
+        port : 7777,
+        hot : true
     }
-}
+ };
