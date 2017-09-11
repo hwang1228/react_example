129a3563a9ad5776f9651edb8cb2bfd6c864f95b
Hyungseob Won <stepanowon@hotmail.com>
2017-09-11 20:00:40 +0900

    Update webpack.production.config.js

--- ch00/example00_2/webpack.production.config.js
+++ ch00/example00_2/webpack.production.config.js
@@ -3,44 +3,56 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    devtool : 'source-map',
-    entry : __dirname + '/app/main.js',
-    output : {
-        path : __dirname + '/public',
-        filename : '[name]-[hash].js'
+    entry: __dirname + '/app/main.js', 
+    output: { 
+        path: __dirname + '/public', 
+        filename: '[name]-[hash].js' 
     },
-    module : {
-        loaders : [
-            { test:/\.json$/, loader : 'json-loader' },
-            { test:/\.js$/, exclude:/node_modules/, loader:'babel-loader' },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
             {
                 test: /\.css$/,
                 use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [ "css-loader?modules", "postcss-loader" ]
+                  fallback: "style-loader?modules",
+                  use: "css-loader?modules"
                 })
+            }
+        ],
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
             }
         ]
     },
     plugins : [
-       new HtmlWebpackPlugin({
-           title : 'webpack 샘플 페이지', 
-           template : __dirname + '/app/index.ejs', 
-           filename : 'index.html',
-           mystring : '<div id="key1">Hello</div>'
-       }),
-       new webpack.BannerPlugin("Date : 2016.09.02"),
-       new webpack.BannerPlugin("Author : Stephen Won"),
-       new webpack.BannerPlugin("Copyright OpenSG inc."),
-       new webpack.HotModuleReplacementPlugin(),
-       new ExtractTextPlugin("[name]-[hash].css"),
-       new webpack.optimize.OccurrenceOrderPlugin(true),
-       new webpack.optimize.UglifyJsPlugin()
-    ],
-    devServer : {
-        contentBase : './public',
-        inline:true,
-        historyApiFallback : true,
-        port : 7777
-    }
-}
+        new HtmlWebpackPlugin({
+            title : 'webpack 샘플 페이지', 
+            template : __dirname + '/app/index.ejs', 
+            filename : 'index.html',
+            mystring : '<div id="key1">Hello</div>'       
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.BannerPlugin("Date : 2017.09.02"),
+        new webpack.BannerPlugin("Author : Stephen Won"),
+        new webpack.BannerPlugin("Copyright OpenSG inc."),
+        new ExtractTextPlugin("[name]-[hash].css"),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+ };
+ 
