caa6494ecb82981d0d8b6a3e0c25d8b5bf770528
Hyungseob Won <stepanowon@hotmail.com>
2017-09-11 20:00:25 +0900

    Update webpack.config.js

--- ch00/example00_2/webpack.config.js
+++ ch00/example00_2/webpack.config.js
@@ -1,55 +1,68 @@
-var WebpackBrowserPlugin = require('webpack-browser-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var WebpackBrowserPlugin = require('webpack-browser-plugin');
 var webpack = require('webpack');
 
 module.exports = {
-    devtool : 'source-map',
-    entry : __dirname + '/app/main.js',
-    output : {
-        path : __dirname + '/public',
-        filename : 'bundle.js'
+    devtool : 'source-map', 
+    entry: __dirname + '/app/main.js', 
+    output: { 
+        path: __dirname + '/public', 
+        filename: 'bundle.js' 
     },
-    module : {
-        rules : [
-            { test:/\.json$/, loader : 'json-loader' },
-            { test:/\.js$/, exclude:/node_modules/, loader:'babel-loader' },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader?modules',
-                    'css-loader?modules',
+                    { loader: "style-loader" },
+                    { loader: 'css-loader', options: { modules: 1 } },
                     {
-                        loader : 'postcss-loader',
-                        options : {
-                            plugins : function() {
-                                return [
-                                    require('precss'),
-                                    require('autoprefixer')
-                                ];
-                            }
+                        loader: 'postcss-loader',
+                        options: {
+                          plugins: (loader) => [
+                            require('autoprefixer')(),
+                            require('precss')()
+                          ]
                         }
                     }
                 ]
             }
+        ],
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }
         ]
     },
     plugins : [
-       new HtmlWebpackPlugin({
-           title : 'webpack 샘플 페이지', 
-           template : __dirname + '/app/index.ejs', 
-           filename : 'index.html',
-           mystring : '<div id="key1">Hello</div>'
+        new HtmlWebpackPlugin({
+            title : 'webpack 샘플 페이지', 
+            template : __dirname + '/app/index.ejs', 
+            filename : 'index.html',
+            mystring : '<div id="key1">Hello</div>'       
        }),
-       new WebpackBrowserPlugin(),
-       new webpack.BannerPlugin("Date : 2016.09.02"),
+       new webpack.BannerPlugin("Date : 2017.09.02"),
        new webpack.BannerPlugin("Author : Stephen Won"),
        new webpack.BannerPlugin("Copyright OpenSG inc."),
-       new webpack.HotModuleReplacementPlugin()
+       new webpack.HotModuleReplacementPlugin()       
     ],
     devServer : {
         contentBase : './public',
-        inline:true,
-        historyApiFallback : true,
-        port : 7777
+        inline:true, 
+        historyApiFallback :true,
+        port : 7777,
+        hot : true
     }
-}
+ };
+ 
