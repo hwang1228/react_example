1ecde8d48f4da4ed47fcf4a0711cb8cdba6df353
Hyungseob Won <stepanowon@hotmail.com>
2017-09-12 08:10:10 +0900

    Update webpack.production.config.js

--- ch01/kanban01/webpack.production.config.js
+++ ch01/kanban01/webpack.production.config.js
@@ -3,41 +3,56 @@ var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry : __dirname + '/source/App.js',
-    output : {
-        path : __dirname + '/public',
-        filename : '[name]-[hash].js'
+    entry: __dirname + '/source/Appjs', 
+    output: { 
+        path: __dirname + '/public', 
+        filename: '[name]-[hash].js' 
     },
-    module : {
-        loaders : [
-            { test:/\.json$/, loader : 'json-loader' },
-            { test:/\.js$/, exclude:/node_modules/, loader:'babel-loader' },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
             {
                 test: /\.css$/,
-                use: [
-                    'style-loader?modules',
-                    'css-loader?modules',
-                    {
-                        loader : 'postcss-loader',
-                        options : {
-                            plugins : function() {
-                                return [
-                                    require('precss'),
-                                    require('autoprefixer')
-                                ];
-                            }
-                        }
-                    }
-                ]
+                use: ExtractTextPlugin.extract({
+                  fallback: "style-loader?modules",
+                  use: "css-loader?modules"
+                })
+            }
+        ],
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
             }
         ]
     },
     plugins : [
-       new webpack.BannerPlugin("Date : 2016.09.02"),
-       new webpack.BannerPlugin("Author : Stephen Won"),
-       new webpack.BannerPlugin("Copyright OpenSG inc."),
-       new ExtractTextPlugin("[name]-[hash].css"),
-       new webpack.optimize.OccurrenceOrderPlugin(true),
-       new webpack.optimize.UglifyJsPlugin()
+        new HtmlWebpackPlugin({
+            title : 'webpack 샘플 페이지', 
+            template : __dirname + '/app/index.ejs', 
+            filename : 'index.html',
+            mystring : '<div id="key1">Hello</div>'       
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        new webpack.BannerPlugin("Date : 2017.09.02"),
+        new webpack.BannerPlugin("Author : Stephen Won"),
+        new webpack.BannerPlugin("Copyright OpenSG inc."),
+        new ExtractTextPlugin("[name]-[hash].css"),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin()
     ]
-}
+ };
+ 
